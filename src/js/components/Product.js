f31f47b651527fd4346f5703b0441ebb900382f5
Julia Glinka <julia.w.glinka@gmail.com>
2023-11-02 15:13:01 +0100

    Comment console logs

--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -5,8 +5,8 @@ class Product {
   constructor(id, data){
     const thisProduct  = this;
 
-    thisProduct.id = id;        //id produktu z danych pobranych z serwera
-    thisProduct.data = data;    //dane produktu
+    thisProduct.id = id;        //id of product from data downloaded from server
+    thisProduct.data = data;    //product data
 
     thisProduct.renderInStore();
   }
@@ -19,21 +19,21 @@ class Product {
     
     /* create element using utils.createElementFromHTML */
     thisProduct.element = utils.createDOMFromHTML(generatedHTML);
-    console.log('element',thisProduct.element);
+    //console.log('element',thisProduct.element);
 
     /* create clone of this element */
     const cloneElement = thisProduct.element.cloneNode(true);
-    console.log('clone',cloneElement);
+    //console.log('clone',cloneElement);
 
     /* find store containers */
     thisProduct.storeContainers = document.querySelectorAll(select.containerOf.store);
-    console.log('storeContainers', thisProduct.storeContainers);
+    //console.log('storeContainers', thisProduct.storeContainers);
 
     thisProduct.homePageStore = thisProduct.storeContainers[0];
     thisProduct.productsPageStore = thisProduct.storeContainers[1];
 
-    console.log('0',thisProduct.homePageStore);
-    console.log('1',thisProduct.productsPageStore);
+    //console.log('0',thisProduct.homePageStore);
+    //console.log('1',thisProduct.productsPageStore);
     
     /* add element to subpages home and pdoducts */
     thisProduct.homePageStore.appendChild(thisProduct.element); 
